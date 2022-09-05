95ab92445f57b09eb93ee8ef26e48b00f8656d8a
HexRabbit <h3xrabbit@gmail.com>
2022-09-05 15:38:46 +0000

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
-# based on http://kaibun.net/blog/2011/08/07/how-to-host-a-jekyll-app-on-github-pages-with-plugins/
-
+git fetch
 git checkout gh-pages
+
 rm -rf *
-for file in `git ls-tree master www -r --name-only`; do
-  `git checkout master $file` && cp -r www/* . && rm -rf www
-done
-touch .nojekyll
+echo 'syscall.hexrabbit.io' > CNAME
+
+git checkout master www
+mv www/* .
+rm -rf www
+
 git add -A .
 git commit -m "static content update at `git rev-parse --short HEAD`"
 git push origin gh-pages
